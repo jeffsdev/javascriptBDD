a73734165c4e27ddcece779be4ac2b8dfb197752
Jeff Seymour <seym1585@gmail.com>
2015-10-23 23:09:13 -0700

    add third spec, and code that will make it pass.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@ var song = function(bottleNumber) {
       verse = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
     } else if (i === 1) {
       verse = i + " bottle of beer on the wall, " + i + " bottle of beer. Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.";
-    }
+    } else if (i === 2) {
+      verse = i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i-1) + " bottle of beer on the wall.";
+    }    
     return verse;
   };
 };
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe("song", function() {
   it("will return the second-to-last verse of the song, if the number of bottles equals 1.", function() {
     expect(song(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.");
   });
+  it("will return the third-to-last verse of the song, if the number of bottles equals 2.", function() {
+    expect(song(2)).to.equal("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.");
+  });
 });

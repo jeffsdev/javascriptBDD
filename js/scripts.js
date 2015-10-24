3c707bde6a4f88736161d715c6273db14ea5264d
Jeff Seymour <seym1585@gmail.com>
2015-10-23 22:50:53 -0700

    add second spec test, and the code to pass it

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ var song = function(bottleNumber) {
   for (var i = bottleNumber; i >= 0; i--) {
     if (i === 0) {
       verse = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
+    } else if (i === 1) {
+      verse = i + " bottle of beer on the wall, " + i + " bottle of beer. Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.";
     }
     return verse;
   };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
 describe("song", function() {
-  it("will return the final verse of the song, if the number of bottles equals 0", function() {
+  it("will return the final verse of the song, if the number of bottles equals 0.", function() {
     expect(song(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+  it("will return the second-to-last verse of the song, if the number of bottles equals 1.", function() {
+    expect(song(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.");
   });
 });
